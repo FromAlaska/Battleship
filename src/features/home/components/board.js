7aa0453a05dd6e6c342935d1aa9794879a50bf06
ccfortenberry <cmnky@hotmail.com>
2018-02-21 09:32:39 -0900

    Added name property to ships, changed ply from object to array, getShip now returns, isSunk now returns, fire now checks if a ship is sunk.

--- src/features/home/components/board.js
+++ src/features/home/components/board.js
@@ -1,17 +1,18 @@
 // Make ships for battleship
+var name;	// Name of the ship
 var len;	// Length of the ship
 var pos;	// Position of the ship
 			// Will ships need more logic?
 
 // The ships
-var acCarrier = {len: 5, pos: []};
-var battleship = {len: 4, pos: []};
-var submarine = {len: 3, pos: []};
-var destroyer = {len: 3, pos: []};
-var patrolBoat = {len: 2, pos: []};
+var acCarrier = {name: "Aircraft Carrier", len: 5, pos: []};
+var battleship = {name: "Battleship", len: 4, pos: []};
+var submarine = {name: "Submarine", len: 3, pos: []};
+var destroyer = {name: "Destroyer", len: 3, pos: []};
+var patrolBoat = {name: "Patrol Boat", len: 2, pos: []};
 
 // The one who owns the ships
-var ply = {acCarrier, battleship, submarine, destroyer, patrolBoat};
+var ply = [acCarrier, battleship, submarine, destroyer, patrolBoat]; // Can we use only one?
 
 // Make a new battleship board
 var colA = new Array(10);
@@ -28,8 +29,10 @@ var bsBoard = [colA, colB, colC, colD, colE, colF, colG, colH, colI, colJ];
 
 // Place a ship on the board
 function placeShip(ship, column, row, orient) {
-	// Get ship head and set the ships points to the
-	// corresponding points
+	// Check to see if there is a conflict
+	
+	// If not, get ship head and set the ships 
+	// points to the corresponding points
 	if (orient == "horizontal") {
 		for (i = 0; i < ship.len; i++) {
 			// Mark spot as taken
@@ -54,9 +57,12 @@ function getShip(column, row) {
 	// Ship positions should be stored as a string of two ints
 	// Find ship pos that matches passed coord. and return
 	// that ship.
-	for (i = 0; i < Object.keys(ply).length; i++) {
-		for (j = 0; j < Object.keys(ply[i]).length; j++) {
-			
+	var shipPos = ''+column+row
+	for (i = 0; i < ply.length; i++) {
+		for (j = 0; j < ply.length; j++) {
+			if (shipPos == ply[i].pos[j]) {
+				return ply[i];
+			}
 		}
 	}
 }
@@ -79,8 +85,14 @@ function atPos(column, row) {
 }
 
 // Checking to see if a ship has been sunk
-function isSunk() {
+function isSunk(ship) {
 	// Return whether or not a ship has been sunk
+	for (i = 0; i < ship.pos.length; i++) {
+		if (ship.pos[i] != 2) {
+			return false;
+		}
+	}
+	return true;
 }
 
 // Fire the main guns!
@@ -92,7 +104,12 @@ function fire(column, row) {
 		// Shot hit
 		bsBoard[column][row] = 2;
 		// Check to see if this sinks the ship
-		return "Hit success!";
+		if (isSunk(getShip(column, row))) {
+			return "You sunk the ship!";
+		} else {
+			// If not we only hurt it
+			return "Hit success!";
+		}
 	} else {
 		// This position has already been shot at
 		// Return error and attempt to re-fire
